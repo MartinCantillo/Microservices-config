f05fe597d41c8c2ccb5944320bf67b8cbe6ee0e4
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-11-15 20:52:24 -0500

    first commit

---
+++ src/main/java/com/martin/springcloud/msvc/products/msvcproducts/MsvcProductsApplication.java
@@ -0,0 +1,13 @@
+package com.martin.springcloud.msvc.products.msvcproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcProductsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/martin/springcloud/msvc/products/msvcproducts/MsvcProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.springcloud.msvc.products.msvcproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
