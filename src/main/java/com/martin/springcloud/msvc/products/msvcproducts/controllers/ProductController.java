c341f087b159cfc725f93d5a1b469eb99d96c200
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-11-15 22:09:29 -0500

    Adding controller

---
+++ src/main/java/com/martin/springcloud/msvc/products/msvcproducts/controllers/ProductController.java
@@ -0,0 +1,38 @@
+package com.martin.springcloud.msvc.products.msvcproducts.controllers;
+
+
+import com.martin.springcloud.msvc.products.msvcproducts.entities.Product;
+import com.martin.springcloud.msvc.products.msvcproducts.services.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity< List<Product>> list() {
+        return ResponseEntity.ok().body(productService.findAll());
+    }
+
+    @GetMapping("/{id")
+    public ResponseEntity<Product>details(@PathVariable Long id) {
+        Optional<Product> product = productService.findById(id);
+        if (product.isPresent()) {
+            return ResponseEntity.ok(product.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
